572ef46dfdae7358e2d5d5bd9b937819ee918658
zhangxin <1053753539@qq.com>
2017-08-12 17:31:18 +0800

    work

---
+++ six.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Administrator on 2017/8/12.
+ */
+var filterFn = require('./six1.js')
+var dir = process.argv[2]
+var filterStr = process.argv[3]
+filterFn(dir, filterStr, function (err, list) {
+    if (err)
+        return console.error('There was an error:', err)
+    list.forEach(function (file) {
+        console.log(file)
+    })
+})
